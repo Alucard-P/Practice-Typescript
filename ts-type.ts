e3adca48f8da6464b2e62f3f752c69d8feae9692
Alucard-P <paulmct93@gmail.com>
2023-05-26 23:43:08 -0500

    add types

---
+++ ts-type.ts
@@ -0,0 +1,24 @@
+console.log("hola");
+
+let velocity_light: number = 300_000;
+let favourite_food: string = "ceviche";
+let is_single: boolean = false;
+
+let new_variable: string;
+new_variable = "soy una nueva variable";
+
+const new_function = (a: number, b: number) => {
+  return `Soy una funcion y los datos que me diste son : ${a} y ${b} `;
+};
+
+let animals: string[] = ["cat", "dog", "mouse"];
+
+console.log(velocity_light);
+console.log(favourite_food);
+console.log(is_single);
+
+console.log(new_variable);
+
+console.log(new_function(2, 20));
+
+console.log(animals);
