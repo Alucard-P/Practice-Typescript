32d5b5a356a1ce2cdb0ae9a700e968fe45b6cf50
Alucard-P <paulmct93@gmail.com>
2023-05-27 19:23:14 -0500

    practice finished

--- ts-type.ts
+++ ts-type.ts
@@ -12,6 +12,71 @@ const new_function = (a: number, b: number) => {
 };
 
 let animals: string[] = ["cat", "dog", "mouse"];
+let numbers: number[] = [71, 23, 1];
+let checks: boolean[] = [];
+let nums2: Array<number> = [];
+
+// numbers.map(x=> x.) // El autocompletado sugiere metodos del tipo de dato
+
+//tuplas = Es una variable que contiene un set de datos que se tienen ordenados
+
+let tupla: [number, string] = [1, "Soy yo"];
+let tupla2: [number, string[]] = [1, ["pequeño", "mediano", "grande"]];
+
+const chica = "s";
+const mediana = "m";
+const grande = "l";
+const extragrande = "xl";
+
+//PascalCase
+
+enum Talla {
+  Chica = "s",
+  Mediana = "m",
+  Grande = "l",
+  Extragrande = "xl",
+}
+
+const pruebaEnum = Talla.Chica;
+
+const enum LoadingState {
+  Idle,
+  Loading,
+  Success,
+  Error,
+}
+
+const estado = LoadingState.Idle;
+
+type Address = {
+  number: number;
+  street: string;
+  country: string;
+};
+
+type parametros = {
+  readonly id: number;
+  name?: string;
+  talla: Talla;
+  address: Address;
+};
+
+const object: parametros = {
+  id: 1,
+  name: "Paul",
+  talla: Talla.Chica,
+  address: {
+    number: 23,
+    street: "Los olivos",
+    country: "Peru",
+  },
+};
+
+const arr: parametros[] = [];
+
+console.log(pruebaEnum);
+
+console.log(estado);
 
 console.log(velocity_light);
 console.log(favourite_food);
